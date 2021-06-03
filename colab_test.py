49eca862524e7870b9064a8a9fa1c9055b539bd3
sswwd95 <sswwd95@gmail.com>
2021-06-03 12:26:57 +0900

    commit

--- colab_test.py
+++ colab_test.py
@@ -1,3 +1,8 @@
 # https://colab.research.google.com/drive/19X98nJdmw1p7GCaYRevhwYzClEJyMdbI?usp=sharing
 
 # IGPT 예제
+
+
+# 클러스터
+# https://www.pyimagesearch.com/2014/05/26/opencv-python-k-means-color-clustering/
+# 이미지에서 색상 추출하는 알고리즘
